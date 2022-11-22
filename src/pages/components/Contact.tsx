b6cdb04911eb4f405f4a59ce617557bd1d4aa29e
James Ball <jamie.j.ball@gmail.com>
2022-11-21 19:52:04 -0800

    initial commit

---
+++ src/pages/components/Contact.tsx
@@ -0,0 +1,5 @@
+const Contact = () => {
+  return <div className="text-4xl text-white">Contact</div>;
+};
+
+export default Contact;
---
+++ src/pages/components/Projects.tsx
@@ -0,0 +1,5 @@
+const Projects = () => {
+  return <div className="w-full">Projects</div>;
+};
+
+export default Projects;
