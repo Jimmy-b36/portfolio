034839d09ac593535e9130df65b46d84b4e1ee6a
James Ball <jamie.j.ball@gmail.com>
2022-12-01 14:30:39 -0800

    add interface for titles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,34 @@
 import { type NextPage } from "next";
 import InfoBar from "./components/InfoBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Skills from "./components/Skills";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
-const TITLES = [
-  { title: "About", color: "#2A9D8F", page: <About /> },
-  { title: "Projects", color: "#E9C46A", page: <Projects /> },
+interface ITitles {
+  title: string;
+  color: string;
+  page: JSX.Element;
+}
+const TITLES: ITitles[] = [
+  {
+    title: "About",
+    color: "#2A9D8F",
+    page: <About />,
+  },
+  {
+    title: "Projects",
+    color: "#E9C46A",
+    page: <Projects />,
+  },
   { title: "Contact", color: "#F4A261", page: <Contact /> },
   { title: "Skills", color: "#E76F51", page: <Skills /> },
 ];
 
 const Home: NextPage = () => {
   const [pageSelected, setPageSelected] = useState("Home");
+
   // if the page selected is not home or that page itself transform button to other side of page
   return (
     <div className="flex bg-slate-900 lg:flex-row">
