574a64dd379a1b90864755bc866c51df4f925aea
James Ball <jamie.j.ball@gmail.com>
2022-12-17 14:32:03 -0800

    add fade effect

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,28 +12,30 @@ const About = () => {
   return (
     <>
       {isExpanded && (
-        <Fade left>
-          <div className="w-auto flex-grow items-center justify-center transition-all lg:flex-col">
-            <h1 className="font-bold text-gray-200 lg:m-5 lg:text-5xl">
-              About
-            </h1>
-            <div className="flex items-center lg:flex-row">
-              <Image
-                src="https://github.com/Jimmy-b36/portfolio/blob/main/public/images/ProfilePic.jpg?raw=true"
-                alt="Picture of the author"
-                width={600}
-                height={600}
-                className="w-auto rounded-full md:h-[300px] xl:h-[600px]"
-              />
-              <p className="ml-10 w-64 whitespace-pre-line text-gray-200">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-                dicta eum! Vel odio iste sed at possimus ad ex quibusdam!
-                Obcaecati, unde. Dignissimos dicta reiciendis ratione ut
-                molestias, ea culpa.
-              </p>
+        <div className="flex items-center justify-center">
+          <Fade left>
+            <div className="w-auto transition-all lg:flex-col">
+              <h1 className="bg-gradient-to-r from-blue-200 to-pink-600 bg-clip-text text-center text-6xl font-extrabold text-transparent lg:m-5 ">
+                About
+              </h1>
+              <div className="flex items-center lg:flex-row">
+                <Image
+                  src="https://github.com/Jimmy-b36/portfolio/blob/main/public/images/ProfilePic.jpg?raw=true"
+                  alt="Picture of the author"
+                  width={600}
+                  height={600}
+                  className="w-auto rounded-full md:h-[300px] xl:h-[600px]"
+                />
+                <p className="ml-10 w-64 whitespace-pre-line text-gray-200">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
+                  dicta eum! Vel odio iste sed at possimus ad ex quibusdam!
+                  Obcaecati, unde. Dignissimos dicta reiciendis ratione ut
+                  molestias, ea culpa.
+                </p>
+              </div>
             </div>
-          </div>
-        </Fade>
+          </Fade>
+        </div>
       )}
     </>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -27,6 +27,7 @@ const Projects = () => {
     ],
     fetcher
   );
+
   useEffect(() => {
     const x = setTimeout(() => {
       setIsExpanded(true);
@@ -36,37 +37,47 @@ const Projects = () => {
 
   if (error) return <div>failed to load</div>;
   return (
-    <div className="flex flex-wrap justify-center ">
-      <Fade left>
-        {data &&
-          isExpanded &&
-          data.data.map((item: IProjects) => (
-            <a
-              className={`m-10 flex flex-col items-center justify-center rounded-lg bg-slate-500 drop-shadow-[5px_5px_16px_5px_rgba(129,129,129,1)] `}
-              style={{
-                boxShadow: "31px 31px 63px #060911, -31px -31px 63px #182543",
-              }}
-              href={item.attributes.URL}
-              target="_blank"
-              rel="noreferrer"
-              key={item.id}
-            >
-              {" "}
-              <p className="m-2 text-2xl font-bold text-white ">
-                {item.attributes.title}
-              </p>{" "}
-              <p className="m-2 text-white">{item.attributes.description}</p>{" "}
-              <Image
-                src={`http://localhost:1337${item.attributes.image.data.attributes.url}`}
-                width={500}
-                height={600}
-                alt="test"
-                className="rounded-b-lg"
-              />
-            </a>
-          ))}
-      </Fade>
-    </div>
+    <>
+      {isExpanded && (
+        <Fade left>
+          <div className="flex flex-wrap justify-center">
+            <h1 className="w-full bg-gradient-to-r from-blue-200 to-pink-600 bg-clip-text text-center text-6xl font-extrabold text-transparent">
+              Projects
+            </h1>
+            {data &&
+              data.data.map((item: IProjects) => (
+                <a
+                  className={`m-10 flex h-auto w-1/4 flex-col items-center justify-center rounded-lg bg-slate-500 drop-shadow-[5px_5px_16px_5px_rgba(129,129,129,1)] `}
+                  style={{
+                    boxShadow:
+                      "31px 31px 63px #060911, -31px -31px 63px #182543",
+                  }}
+                  href={item.attributes.URL}
+                  target="_blank"
+                  rel="noreferrer"
+                  key={item.id}
+                >
+                  {" "}
+                  <p className="break normal m-2 text-2xl font-bold text-white">
+                    {item.attributes.title}
+                  </p>{" "}
+                  <p className="m-2 text-white">
+                    {item.attributes.description}
+                  </p>{" "}
+                  <Image
+                    src={`http://localhost:1337${item.attributes.image.data.attributes.url}`}
+                    width={400}
+                    height={300}
+                    alt="test"
+                    style={{ objectFit: "contain" }}
+                    className="rounded-b-lg"
+                  />
+                </a>
+              ))}
+          </div>
+        </Fade>
+      )}
+    </>
   );
 };
 
