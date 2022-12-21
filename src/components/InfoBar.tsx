257a5174555e29e2405643a453aada790736bfd3
James Ball <jamie.j.ball@gmail.com>
2022-12-21 14:26:08 -0800

    feat: click sidebar agin brings you home

--- src/components/InfoBar.tsx
+++ src/components/InfoBar.tsx
@@ -4,15 +4,20 @@ interface IInfoBar {
   title: string;
   color: string;
   setPageSelected: Dispatch<SetStateAction<string>>;
+  pageSelected: string;
 }
 
-const InfoBar = ({ title, color, setPageSelected }: IInfoBar) => {
+const InfoBar = ({ title, color, setPageSelected, pageSelected }: IInfoBar) => {
   return (
     <div className="flex min-w-full xl:flex-row">
       <button
         className="flex w-screen items-center justify-center border border-black lg:h-screen lg:w-20 xl:h-screen xl:w-20"
         style={{ backgroundColor: `${color}` }}
-        onClick={() => setPageSelected(title)}
+        onClick={() =>
+          pageSelected === title
+            ? setPageSelected("Home")
+            : setPageSelected(title)
+        }
         value={title}
       >
         <p className="text-5xl font-bold lg:-rotate-90 xl:-rotate-90">
