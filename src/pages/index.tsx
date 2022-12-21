9ca12f75df8ecbcb90fc4643d7e3c6cf0ff8d4f3
James Ball <jamie.j.ball@gmail.com>
2022-12-21 14:27:37 -0800

    feat: create new Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { type NextPage } from "next";
 import InfoBar from "../components/InfoBar";
-import { useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Skills from "../components/Skills";
 import About from "../components/About";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
+import Home from "../components/Home";
 
 interface ITitles {
   title: string;
@@ -19,19 +20,20 @@ const TITLES: ITitles[] = [
   { title: "Contact", color: "#F4A261", page: <Contact /> },
 ];
 
-const Home: NextPage = () => {
+const Index: NextPage = () => {
   const [pageSelected, setPageSelected] = useState("Home");
 
   // if the page selected is not home or that page itself transform button to other side of page
   return (
     <div className="flex min-h-screen flex-col bg-slate-900  lg:flex-row xl:flex-row">
       {TITLES.map((item, index) => (
-        <>
-          <div key={index}>
+        <Fragment key={index}>
+          <div>
             <InfoBar
               title={item.title}
               color={item.color}
               setPageSelected={setPageSelected}
+              pageSelected={pageSelected}
             />
           </div>
           <div
@@ -44,10 +46,11 @@ const Home: NextPage = () => {
               {pageSelected === item.title && item.page}
             </div>
           </div>
-        </>
+        </Fragment>
       ))}
+      {pageSelected === "Home" && <Home />}
     </div>
   );
 };
 
-export default Home;
+export default Index;
