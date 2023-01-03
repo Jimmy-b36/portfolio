f8c7982d13a14b029b61a78387288c683eacca1c
James Ball <jamie.j.ball@gmail.com>
2023-01-02 17:57:05 -0800

    create custom fade animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        "fade-left": "fade-from-left 0.8s ease-in-out",
+      },
+
+      keyframes: () => ({
+        "fade-from-left": {
+          "0%": { opacity: "0", transform: "translateX(-500px)" },
+          "100%": { opacity: "1", tranform: "translateX(0)" },
+        },
+      }),
       screens: {
         xs: "480px",
         sm: "640px",
