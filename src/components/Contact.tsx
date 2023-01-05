0fd845beb431175153f359e90e6373fb5587befc
James Ball <jamie.j.ball@gmail.com>
2023-01-05 10:52:46 -0800

    message is now required

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -98,6 +98,7 @@ const Contact = () => {
                     className="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
                     placeholder="Leave a comment..."
                     name="user_query"
+                    required
                   ></textarea>
                 </div>
                 {error && (
